adc2ef6beb640755abe8b6134a851bb8aee1d4f2
Maria Gabriela <mgabriela.mgs@outlook.com>
2021-11-24 18:30:53 -0300

    Exercicio 1 - OK

--- API/Controllers/InicializarDadosController.cs
+++ API/Controllers/InicializarDadosController.cs
@@ -21,14 +21,18 @@ namespace API.Controllers
         {
             _context.Categorias.AddRange(new Categoria[]
                 {
-                    new Categoria { CategoriaId = 1, Nome = "Categoria 1" },
+                    new Categoria { CategoriaId = 1, Nome = "Computador" },
+                    new Categoria { CategoriaId = 2, Nome = "Celular" },
                 }
             );
             _context.Produtos.AddRange(new Produto[]
                 {
-                    new Produto { ProdutoId = 1, Nome = "Produto 1", Preco = 1, Quantidade = 1, CategoriaId = 1 },
-                    new Produto { ProdutoId = 2, Nome = "Produto 2", Preco = 2, Quantidade = 2, CategoriaId = 1 },
-                    new Produto { ProdutoId = 3, Nome = "Produto 3", Preco = 3, Quantidade = 3, CategoriaId = 1 },
+                    new Produto { ProdutoId = 1, Nome = "Dell Intel I5", Preco = 1000, Quantidade = 5, CategoriaId = 1 },
+                    new Produto { ProdutoId = 2, Nome = "Lenovo Intel I7", Preco = 2000, Quantidade = 10, CategoriaId = 1 },
+                    new Produto { ProdutoId = 3, Nome = "Acer AMD Ryzen 9", Preco = 3000, Quantidade = 13, CategoriaId = 1 },
+                    new Produto { ProdutoId = 4, Nome = "Iphone 12", Preco = 1500, Quantidade = 25, CategoriaId = 2 },
+                    new Produto { ProdutoId = 5, Nome = "Samsumg S20", Preco = 3000, Quantidade = 3, CategoriaId = 2 },
+                    new Produto { ProdutoId = 6, Nome = "Xiaomi 11 Ultra", Preco = 5000, Quantidade = 10, CategoriaId = 2 },
                 }
             );
             _context.SaveChanges();
